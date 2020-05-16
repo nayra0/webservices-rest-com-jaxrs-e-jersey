6ce8734ec50090fa47d62993641428c62cd0398c
Nayra Oliveira <nayrabiaoliveira@gmail.com>
2020-05-15 21:49:56 -0300

    Criado servidor e resource para prover o recurso carrinho

---
+++ src/main/java/br/com/alura/loja/servidor/Servidor.java
@@ -0,0 +1,21 @@
+package br.com.alura.loja.servidor;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class Servidor {
+
+	public static void main(String[] args) throws IOException {
+		URI uri = URI.create("http://localhost:8080");
+		ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
+		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config  );
+		System.out.println("Rodando");
+		System.in.read();
+		server.stop();
+		
+	}
+}
